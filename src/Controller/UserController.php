5d7845e2f22e2c8b5f99531f1ffff6e035e2b24e
t0nn1x <antoonhrobust@gmail.com>
2025-04-26 03:41:55 +0200

    feat: Implement role-based access control for user management in UserController

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/api/users')]
 class UserController extends AbstractApiController
@@ -32,36 +33,42 @@ class UserController extends AbstractApiController
     }
 
     #[Route('', name: 'api_users_list', methods: ['GET'])]
+    #[IsGranted('ROLE_ADMIN', message: 'Only administrators can view all users')]
     public function list(Request $request): JsonResponse
     {
         return $this->getCollection($request);
     }
 
     #[Route('/{id}', name: 'api_users_get', methods: ['GET'])]
+    #[IsGranted('ROLE_ADMIN', message: 'Only administrators can view user details')]
     public function get(int $id): JsonResponse
     {
         return $this->getItem($id);
     }
 
     #[Route('', name: 'api_users_create', methods: ['POST'])]
+    #[IsGranted('ROLE_ADMIN', message: 'Only administrators can create users')]
     public function create(Request $request): JsonResponse
     {
         return $this->createItem($request);
     }
 
     #[Route('/{id}', name: 'api_users_update', methods: ['PUT'])]
+    #[IsGranted('ROLE_ADMIN', message: 'Only administrators can update users')]
     public function update(Request $request, int $id): JsonResponse
     {
         return $this->updateItem($request, $id);
     }
 
     #[Route('/{id}', name: 'api_users_patch', methods: ['PATCH'])]
+    #[IsGranted('ROLE_ADMIN', message: 'Only administrators can modify users')]
     public function patch(Request $request, int $id): JsonResponse
     {
         return $this->patchItem($request, $id);
     }
 
     #[Route('/{id}', name: 'api_users_delete', methods: ['DELETE'])]
+    #[IsGranted('ROLE_ADMIN', message: 'Only administrators can delete users')]
     public function delete(int $id): JsonResponse
     {
         return $this->deleteItem($id);
